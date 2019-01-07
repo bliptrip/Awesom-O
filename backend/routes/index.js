de6e08b02f56b51b1a1619dccfeb7529fd7287b6
Andrew Maule <maule2@wisc.edu>
2019-01-07 08:43:27 -0600

    Adding routes for db and camera access.

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,5 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/api', require('./api'));
+router.use('/db', require('./db'));
+router.use('/camera', require('./camera'));
 
 module.exports = router;
