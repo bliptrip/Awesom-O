093527ba69b8ab4b22133bff574acca654930648
Andrew Maule <maule2@wisc.edu>
2019-01-23 20:06:31 -0600

    Adding comments

--- backend/models/ExperimentConfig.js
+++ backend/models/ExperimentConfig.js
@@ -3,14 +3,17 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
 const PlateSchema = new Schema({
-    row: Number,
-    col: Number,
-    file_prefix: Number,
-    meta: {type: Map, of: String}
+    row: Number, //Row of plate
+    col: Number, //Column of plate
+    meta: {type: Map, of: String} //Experimental metadata fields associated with plate
 });
 
 const ExperimentConfigSchema = new Schema({
-    version: Number,
+    version: Number, //Version ID
+    datetime: Boolean, //Append datetime suffix to files as they are created
+    rename: Boolean, //Rename files from default
+    image_meta: Boolean, //Embed metadata in image file, if supported
+    filename_fields: [String], //Which metadata fields include in filename, in order listed
     config: [PlateSchema]
 });
 
