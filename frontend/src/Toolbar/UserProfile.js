80bba7cf47ce8a984f6e6ae37763b0401fbae9aa
Andrew Maule <maule2@wisc.edu>
2019-02-03 21:15:06 -0600

    Adding AGPL license notice banner to all javascript files.

--- frontend/src/Toolbar/UserProfile.js
+++ frontend/src/Toolbar/UserProfile.js
@@ -1,3 +1,24 @@
+/**************************************************************************************
+This file is part of Awesom-O, an image acquisition and analysis web application,
+consisting of a frontend web interface and a backend database, camera, and motor access
+management framework.
+
+Copyright (C)  2019  Andrew F. Maule
+
+Awesom-O is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Awesom-O is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this Awesom-O.  If not, see <https://www.gnu.org/licenses/>.
+**************************************************************************************/
+
 import './UserProfile.scss'
 import React, { useState } from 'react';
 
